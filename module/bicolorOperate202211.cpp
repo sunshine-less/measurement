bfd1922ce6780b695bc14d32c392d67d14c77a70
look <1147057783@qq.com>
2022-11-10 01:12:58 +0800

    fighting

--- module/bicolorOperate202211.cpp
+++ module/bicolorOperate202211.cpp
@@ -1,12 +1,47 @@
+//we need some head files. include goverment and another company and other people .
 #include <iostream>
+#include <fstream>
+#include "../head_file/rapidjson/document.h"
+#include "../head_file/rapidjson/writer.h"
+#include "../head_file/rapidjson/stringbuffer.h"
+
 using namespace std;
+using namespace rapidjson;
+
+
+void print01();
+
 int main(){
-    cout<<"hello hero"<<endl;
-    int a = 2;
-    int b = 3;
-    cout<<a+b<<endl;
+    cout<<"get some data from json file."<<endl;
+    print01();
     return 0;
 }
 
+void print01(){
+    cout<<"slk"<<endl;
+}
+string readFile(){
+    ifstream file;
+    file.open("../data/bicolor/s1.json",ios_base::in);
+    if(!file.is_open()){
+	cout<<"open failure"<<endl;
+    }
+    /*
+    stringstream ss;
+    ss<<file.rdbuf();
+    string str = ss.str();
+    cout<<str<<endl;
+    */
+    string str;
+    return str;
+}
 
-//
+/***read from file , storage in [code,blue] ,
+ *
+ *
+ *
+ *
+ */
+
+
+
